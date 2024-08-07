36ceaa70b85100272dada2e5dc5e755f685fbb37
v420v <ibuki420v@gmail.com>
2024-08-07 12:18:19 +0900

    functions.php 引数や戻り値の型を指定

--- test_app/functions.php
+++ test_app/functions.php
@@ -1,18 +1,19 @@
 <?php
 
+declare(strict_types=1);
 require_once('connection.php');
 
 /*
     createData 関数は引数で受け取った連想型配列のcontent keyのvalueをcreateTodoData関数に渡してます。
 */
-function createData($post) {
+function createData(array $post): void {
     createTodoData($post['content']);
 }
 
 /*
     getTodoList 関数は、getAllRecords 関数を呼び出し、その戻り値である配列型のtodosレコードを返しています。
 */
-function getTodoList() {
+function getTodoList(): array {
     return getAllRecords();
 }
 
